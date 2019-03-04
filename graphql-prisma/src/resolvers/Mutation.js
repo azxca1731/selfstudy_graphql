4238182faa64f6ecb80748106d1f153adb4be34c
azxca1731 <azxca1731@gmail.com>
2019-03-05 02:07:44 +0900

    delete comment

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -27,28 +27,6 @@ const Mutation = {
         );
 
         return deletedUser;
-        // const userIndex = db.users.findIndex(user => user.id === args.id);
-
-        // if (userIndex === -1) {
-        //     throw new Error("User not found");
-        // }
-
-        // const deletedUsers = db.users.splice(userIndex, 1);
-
-        // db.posts = db.posts.filter(post => {
-        //     const match = post.author === args.id;
-
-        //     if (match) {
-        //         db.comments = db.comments.filter(
-        //             comment => comment.post !== post.id
-        //         );
-        //     }
-
-        //     return !match;
-        // });
-        // db.comments = db.comments.filter(comment => comment.author !== args.id);
-
-        // return deletedUsers[0];
     },
     async updateUser(parent, args, { prisma }, info) {
         const idExist = await prisma.exists.User({ id: args.id });
@@ -66,32 +44,6 @@ const Mutation = {
         );
 
         return updatedUser;
-        // const { id, data } = args;
-        // const user = db.users.find(user => user.id === id);
-
-        // if (!user) {
-        //     throw new Error("User not found");
-        // }
-
-        // if (typeof data.email === "string") {
-        //     const emailTaken = db.users.some(user => user.email === data.email);
-
-        //     if (emailTaken) {
-        //         throw new Error("Email taken");
-        //     }
-
-        //     user.email = data.email;
-        // }
-
-        // if (typeof data.name === "string") {
-        //     user.name = data.name;
-        // }
-
-        // if (typeof data.age !== "undefined") {
-        //     user.age = data.age;
-        // }
-
-        // return user;
     },
     async createPost(parent, args, { prisma, pubsub }, info) {
         const { author: id, ...otheArgs } = args.data;
@@ -120,17 +72,6 @@ const Mutation = {
         return createdPost;
     },
     deletePost(parent, args, { prisma, pubsub }, info) {
-        // const postIndex = db.posts.findIndex(post => post.id === args.id);
-
-        // if (postIndex === -1) {
-        //     throw new Error("Post not found");
-        // }
-        // const postExists = await prisma.exists.Post({ id: args.id });
-
-        // if (!postExists) {
-        //     throw new Error("Post doesn't exist");
-        // }
-
         return prisma.mutation.deletePost(
             {
                 where: {
@@ -139,21 +80,6 @@ const Mutation = {
             },
             info
         );
-
-        // const [post] = db.posts.splice(postIndex, 1);
-
-        // db.comments = db.comments.filter(comment => comment.post !== args.id);
-
-        // if (post.published) {
-        //     pubsub.publish("post", {
-        //         post: {
-        //             mutation: "DELETED",
-        //             data: post
-        //         }
-        //     });
-        // }
-
-        // return post;
     },
     async updatePost(parent, args, { prisma, pubsub }, info) {
         const existPost = await prisma.exists.Post({ id: args.id });
@@ -178,50 +104,6 @@ const Mutation = {
         );
 
         return updatedPost;
-        // const { id, data } = args;
-        // const post = db.posts.find(post => post.id === id);
-        // const originalPost = { ...post };
-
-        // if (!post) {
-        //     throw new Error("Post not found");
-        // }
-
-        // if (typeof data.title === "string") {
-        //     post.title = data.title;
-        // }
-
-        // if (typeof data.body === "string") {
-        //     post.body = data.body;
-        // }
-
-        // if (typeof data.published === "boolean") {
-        //     post.published = data.published;
-
-        //     if (originalPost.published && !post.published) {
-        //         pubsub.publish("post", {
-        //             post: {
-        //                 mutation: "DELETED",
-        //                 data: originalPost
-        //             }
-        //         });
-        //     } else if (!originalPost.published && post.published) {
-        //         pubsub.publish("post", {
-        //             post: {
-        //                 mutation: "CREATED",
-        //                 data: post
-        //             }
-        //         });
-        //     }
-        // } else if (post.published) {
-        //     pubsub.publish("post", {
-        //         post: {
-        //             mutation: "UPDATED",
-        //             data: post
-        //         }
-        //     });
-        // }
-
-        // return post;
     },
     async createComment(parent, args, { prisma, pubsub }, info) {
         const userExists = await prisma.exists.User({ id: args.data.author });
@@ -251,29 +133,6 @@ const Mutation = {
         );
 
         return createdComment;
-        // const userExists = db.users.some(user => user.id === args.data.author);
-        // const postExists = db.posts.some(
-        //     post => post.id === args.data.post && post.published
-        // );
-
-        // if (!userExists || !postExists) {
-        //     throw new Error("Unable to find user and post");
-        // }
-
-        // const comment = {
-        //     id: uuidv4(),
-        //     ...args.data
-        // };
-
-        // db.comments.push(comment);
-        // pubsub.publish(`comment ${args.data.post}`, {
-        //     comment: {
-        //         mutation: "CREATED",
-        //         data: comment
-        //     }
-        // });
-
-        // return comment;
     },
     async deleteComment(parent, args, { prisma, pubsub }, info) {
         const commentExist = await prisma.exists.Comment({ id: args.id });
